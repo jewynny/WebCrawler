f1b3cf77770e1bee52568fad2d939ff82fb04b21
Janelle <jgmh003@gmail.com>
2025-04-30 15:45:56 -0700

    Starting files! moving files from server folder to github folder

---
+++ RedditCrawler/spiders/authors_spider.py
@@ -0,0 +1,14 @@
+# this will be for crawling authors
+
+# pseudo script
+
+# reddit.subreddit("AITA")
+# subreddit.hot(limit=1000)
+# subreddit.new(limit=1000)
+# subreddit.top(limit=1000)
+# grab name
+# post.author.name
+# access their name
+# reddit.redditor("username")
+# redditor.submissions.new(limit=100)
+# redditor.comments.new(limit=100
